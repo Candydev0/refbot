6454f6848285793544e6d35617bb11c4538a6d1a
Candydev0 <105285244+Candydev0@users.noreply.github.com>
2022-05-11 11:19:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
-const fc = require("./fc.js");
-const mysql = require("./mysql.js");
+const com = require('mysql');
+var mysql = com.createConnection({
+    	host     : 'b3rutgfiunn28bzruthy-mysql.services.clever-cloud.com',
+    	database : 'b3rutgfiunn28bzruthy',
+    	user     :'ufac4nge84ipdz1k',
+    	password : 'dtchKN6BAV46GktJ1lZ'
+  });
+
+con.connect(function(err) {
+  if (err) {
+    mysql_data = "Mysql Error On Connection Time:\n"+err; return mysql_data;
+  } else {
+    mysql_data = "true"; return mysql_data;
+  }});
 
 const {
   Telegraf,
